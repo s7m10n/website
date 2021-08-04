f47bfca8727450627016b20c9dcbd0ea853b9d9a
s7m10n <68545070+s7m10n@users.noreply.github.com>
2021-08-04 16:45:20 +0800

    init

---
+++ js/available.js
@@ -0,0 +1,88 @@
+function checkFinance(day, time) {
+  // check if day is saturday or sunday
+  if (day == "Sun" || day == "Sat") {
+    return false;
+  } else {
+    // check if time is within 0800 hrs to 1800 hrs
+    if (time < 8 || time >= 18) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkTechnology(day, time) {
+  // check if day is Mon, Wed, Fri, Sun
+  if (day == "Mon" || day == "Wed" || day == "Fri" || day == "Sun") {
+    return false;
+  } else {
+    // check if time is within 0800 hrs to 1800 hrs
+    if (time < 8 || time >= 18) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function checkMarketing(day, time) {
+  // check if day is sat or sun
+  if (day == "Sat" || day == "Sun") {
+    return false;
+    // check if time is within 1200 hrs to 1800 hrs
+  } else {
+    if (time < 12 || time >= 18) {
+      return false;
+    }
+  }
+  return true;
+}
+
+function status(bool, department) {
+  switch (department) {
+    case "Finance":
+      if (bool) {
+        document.getElementById("availableFinance").innerHTML =
+          '<img src="img/check.svg" alt="Is available" />';
+      } else {
+        document.getElementById("availableFinance").innerHTML =
+          '<img src="img/x.svg" alt="Not available" />';
+      }
+    case "Technology":
+      if (bool) {
+        document.getElementById("availableTechnology").innerHTML =
+          '<img src="img/check.svg" alt="Is available" />';
+      } else {
+        document.getElementById("availableTechnology").innerHTML =
+          '<img src="img/x.svg" alt="Not available" />';
+      }
+    case "Marketing":
+      if (bool) {
+        document.getElementById("availableMarketing").innerHTML =
+          '<img src="img/check.svg" alt="Is available" />';
+      } else {
+        document.getElementById("availableMarketing").innerHTML =
+          '<img src="img/x.svg" alt="Not available" />';
+      }
+  }
+}
+
+var date = new Date().toString();
+date = date.split(" ");
+day = date[0];
+time = date[4].split(":");
+time = time[0];
+availableFinance = checkFinance(day, time);
+availableTechnology = checkTechnology(day, time);
+availableMarketing = checkMarketing(day, time);
+
+status(availableFinance, "Finance");
+status(availableTechnology, "Technology");
+status(availableMarketing, "Marketing");
+// to verify
+console.log(date);
+console.log(day);
+console.log(time);
+
+console.log(availableFinance);
+console.log(availableTechnology);
+console.log(availableMarketing);
