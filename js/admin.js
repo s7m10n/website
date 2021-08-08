bb14306cd1fed43b5cb415a91d7533d6c9880741
s7m10n <68545070+s7m10n@users.noreply.github.com>
2021-08-08 14:20:09 +0800

    add .js to show database using firestore

--- js/admin.js
+++ js/admin.js
@@ -1,3 +1,4 @@
+const userAccordion = document.getElementById("userData");
 const userCollection = db
   .collection("user-data")
   .get()
@@ -11,5 +12,39 @@ const userCollection = db
       const clevel = userData["clevel"];
       const bsector = userData["bsector"];
       console.log(username, csize, clevel, bsector, country, cname);
+
+      // accordion item
+      var userAccordionItem = document.createElement("div");
+      userAccordionItem.className = "accordion-item";
+      userAccordionItem.setAttribute("id", "userData");
+
+      userAccordionItem.innerHTML += `<h2 class="accordion-header" id="${username}">
+        <button
+            class="accordion-button"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#collapse${username}"
+            aria-expanded="false"
+            aria-controls="collapse${username}"
+        >
+            ${cname}
+        </button>
+    </h2>
+    <div
+        id="collapse${username}"
+        class="accordion-collapse collapse show"
+        aria-labelledby="${username}"
+        data-bs-parent="#userData"
+    >
+        <div class="accordion-body">
+            Company Name: ${cname} <br>
+            Company Size: ${csize} <br>
+            Company Level: ${clevel} <br>
+            Business Sector: ${bsector} <br>
+            Country: ${country}
+        </div>
+    </div>`;
+      userAccordion.appendChild(userAccordionItem);
+      console.log(userAccordionItem);
     });
   });
